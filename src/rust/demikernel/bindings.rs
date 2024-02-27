09969eea84728ac8bcb78b0e2f3384b1eb7071ee
Pedro Henrique Penna <ppenna@microsoft.com>
2024-02-27 14:26:23 -0300

    [perf] Enhancement: Improving Perf

--- src/rust/demikernel/bindings.rs
+++ src/rust/demikernel/bindings.rs
@@ -23,10 +23,6 @@ use crate::{
             Socklen,
         },
     },
-    perftools::profiler::{
-        self,
-        PROFILER,
-    },
     runtime::{
         fail::Fail,
         logging,
@@ -108,7 +104,7 @@ pub extern "C" fn demi_init(argc: c_int, argv: *mut *mut c_char) -> c_int {
         for sig in signals.forever() {
             println!("Received signal {:?}", sig);
             #[cfg(feature = "profiler")]
-            profiler::write(&mut std::io::stdout(), None).unwrap();
+            crate::perftools::profiler::write(&mut std::io::stdout(), None).unwrap();
             std::process::exit(0);
         }
     });
